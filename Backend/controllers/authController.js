22b817d78bc954263f6c6fd3ea1fa1f559df58ac
Adityapratap11006 <adityapratap11006@gmail.com>
2026-06-20 19:56:08 +0530

    Initial backend setup with JWT authentication

---
+++ Backend/controllers/authController.js
@@ -0,0 +1,67 @@
+const jwt = require("jsonwebtoken");
+const User=require("../models/User")
+const bcrypt = require("bcryptjs");
+const registerUser = async (req,res) => {
+   try{
+    const {username,email,password}=req.body;
+    
+    const existingUser = await User.findOne({ email });
+
+if(existingUser){
+    return res.status(400).json({
+        message: "User already exists"
+    });
+}
+const hashedPassword = await bcrypt.hash(password, 10);
+    const user =await User.create({
+        username,
+        email,
+   password: hashedPassword
+    });
+    res.status(201).json(user)
+   }catch(err){
+    res.status(500).json({
+        message:err.message
+    });
+   }
+};
+const loginUser = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return res.status(404).json({
+                message: "User does not exist"
+            });
+        }
+
+        const isMatch = await bcrypt.compare(
+            password,
+            user.password
+        );
+
+        if (!isMatch) {
+            return res.status(401).json({
+                message: "Password Incorrect"
+            });
+        }
+const token = jwt.sign(
+    { id: user._id },
+    process.env.JWT_SECRET,
+    { expiresIn: "7d" }
+);
+        return res.status(200).json({
+            message: "Login Successful",
+            token
+        });
+
+    } catch (err) {
+        return res.status(500).json({
+            message: err.message
+        });
+    }
+};
+
+module.exports={registerUser,loginUser};
---
+++ Backend/server.js
@@ -0,0 +1,9 @@
+require("dotenv").config();
+const connectDB=require("./config/db");
+const app=require("./app");
+
+const PORT=process.env.PORT;
+connectDB();
+app.listen(PORT,()=>{
+    console.log(`Server running on ${PORT}`);
+});
